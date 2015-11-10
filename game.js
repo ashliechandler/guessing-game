ff6b75973be63de8b1919b02cec26208510c1c2e
Ashlie Lynn Chandler <ashliechandler@gmail.com>
2015-11-09 19:19:11 -0800

    add a sixth question (one with a numeric number) to the guessing game

--- game.js
+++ game.js
@@ -10,9 +10,11 @@ var guessingGame = [
 ["Did I join a traveling circus when I was 12 to become a trapeze acrobat?", "no", "Correct! I did not join a traveling circus, but I did join a polynesian dance troupe.", "Wrong. Unfortunately, I do not possess the skills to be a trapeze acrobat."],
 ["Did I spend eight months camping and hitchhiking through seven countries in the southern African region?", "yes", "Correct! I traveled through Namibia, Botswana, Zambia, Zimbabwe, Malawi, Mozambique and South Africa.", "Wrong. I am a world traveler goddamnit! Can't you tell?"],
 ["Did I work on a dairy farm in Munich, Germany?", "no", "Correct! I did, however, milk a cow in Kenya once.", "Wrong. I have never been to Germany, but I have plans to travel there next year."],
-["Did I move to Buenos Aires, Argentina to study soccer?", "yes", "Correct! I studied the socioeconomic representation of the Boca Juniors and River Plate soccer teams.", "Wrong. I graduated a semester early from college and moved to Argentina for eight months."],
+["Did I move to Buenos Aires, Argentina to study soccer?", "yes", "Correct! I studied the socioeconomic representations of the Boca Juniors and River Plate soccer teams.", "Wrong. I graduated a semester early from college and moved to Argentina for eight months."],
 ["I used to be a disaster relief worker. How many disasters have I worked?", "5", "You're right! I have worked five disasters.", "Nope, you're wrong."],
+["I worked at a hostel in New Orleans once. How long did I work there?", "6", "Yupp, you're right. I worked at India House hostel for four months.", "Sorry, you're wrong."]
 ];
+
 for (var i = 0; i < guessingGame.length; i++) {
     var answer = prompt(guessingGame[i][0]);
     answer = answer.toLowerCase();
@@ -68,20 +70,37 @@ for (var i = 0; i < guessingGame.length; i++) {
     else if ((answer !== "yes") && (answer !== "no") && (answer !== NaN)) {
         var answer = parseInt(answer);
         console.log(answer);
-        if (answer === guessingGame[4][1]) {
-            alert(guessingGame[4][2]);
+        if (answer === guessingGame[i][1]) {
             correctAnswer.push("<li>" + guessingGame[i][0] + "<b>" + "<br>Your answer: " + answer + "</b>" + "</li>");
             correctCount += 1;
+            var message;
+            var question = guessingGame[i][0];
+            switch (question) {
+                case guessingGame[4][0]:
+                    message = "You're right! I have worked five disasters.";
+                    alert(message);
+                    break;
+                case guessingGame[5][0]:
+                    message = "Yupp, you're right. I worked at India House hostel for four months.";
+                    alert(message);
+                    break;
+            }
         }
-        else if (answer !== guessingGame[4][1]) {
+        else if (answer !== guessingGame[i][1]) {
             console.log(answer);
             wrongAnswer.push("<li>" + guessingGame[i][0] + "<b>" + "<br>Correct answer: " + guessingGame[i][1] + "</b>" + "</li>");
-            if (answer < "5") {
-                alert(guessingGame[4][3] + " That number is too low.");
-            }
-            else if (answer > "5") {
-                alert(guessingGame[4][3] + " That number is too high.");
-            }
+                if ((i === 4) && (answer < "5")) {
+                    alert(guessingGame[4][3] + " That number is too low.");
+                }
+                else if ((i === 4) && (answer > "5")) {
+                    alert(guessingGame[4][3] + " That number is too high.");
+                }
+                else if ((i === 5) && (answer < "6")) {
+                    alert(guessingGame[5][3] + " That number is too low.");
+                }
+                else if ((i === 5) && (answer > "6")) {
+                    alert(guessingGame[5][3] + " That number is too high.");
+                }
         }
     }
     else {
